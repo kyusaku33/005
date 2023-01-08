75e0c6a5a0a8cf84511669ec2e9ad40a2582ecb9
kyusaku33 <kyusakunsqc@yahoo.co.jp>
2023-01-08 18:13:37 +0900

    update

--- img_trans/middleware.py
+++ img_trans/middleware.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.response import TemplateResponse
 from django.core.exceptions import ObjectDoesNotExist
+import traceback
+import logging
 
 class SimpleMiddleware:
 
@@ -28,3 +30,19 @@ class SimpleMiddleware:
             return render(request, 'img_trans/error.html', context)          
         else:
             return render(request, 'img_trans/error.html', context)
+
+
+
+class SimpleMiddleware:
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
+    def process_exception(self, request, exception):
+        err = traceback.format_exc()
+        logging.error(err)
+        logging.error(exception)
